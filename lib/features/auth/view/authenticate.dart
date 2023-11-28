936461846d4b646d5e051be6382d9bd181f1b365
mustapha amin <mustaphaamin2003@gmail.com>
2023-11-28 03:49:05 +0100

    Initial commit

---
+++ lib/features/auth/view/authenticate.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Authentication extends StatelessWidget {
+  const Authentication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/auth/view/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
