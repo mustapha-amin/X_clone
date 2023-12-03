ada4e61c18988cfdd29da7a8a088471e9e453b6b
mustapha amin <mustaphaamin2003@gmail.com>
2023-12-03 12:04:28 +0100

    basic layout of message screen implemented

--- lib/features/messaging/views/message_screen.dart
+++ lib/features/messaging/views/message_screen.dart
@@ -1,11 +1,59 @@
+import 'package:feather_icons/feather_icons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:x_clone/common/x_avatar.dart';
+import 'package:x_clone/utils/utils.dart';
+import '../../../core/core.dart';
 
-class MessageScreen extends ConsumerWidget {
+class MessageScreen extends ConsumerStatefulWidget {
   const MessageScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  ConsumerState<ConsumerStatefulWidget> createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends ConsumerState<MessageScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = ref.watch(userProvider);
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leadingWidth: context.screenWidth * .2,
+          leading: const XAvatar(),
+          title: SearchBar(
+            controller: searchController,
+            constraints: BoxConstraints(
+              minHeight: context.screenHeight * .065,
+              minWidth: context.screenWidth * .6,
+            ),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(
+                  color: Colors.grey[900]!,
+                ),
+              ),
+            ),
+            hintText: "Search Direct Messages",
+            hintStyle: MaterialStatePropertyAll(
+              kTextStyle(13, color: Colors.grey[500]),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FeatherIcons.settings,
+                size: 20,
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
