5e5966a65c2a5fe0dd0cddcee9f7014bc8b6fc6e
mustapha amin <mustaphaamin2003@gmail.com>
2023-12-03 11:51:01 +0100

    explore screen basic layout implemented

--- lib/features/explore/views/explore_screen.dart
+++ lib/features/explore/views/explore_screen.dart
@@ -1,11 +1,71 @@
+import 'package:feather_icons/feather_icons.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:x_clone/utils/utils.dart';
+import '../../../core/core.dart';
 
-class ExploreScreen extends ConsumerWidget {
+class ExploreScreen extends ConsumerStatefulWidget {
   const ExploreScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+  ConsumerState<ConsumerStatefulWidget> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends ConsumerState<ExploreScreen> {
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = ref.watch(userProvider);
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leadingWidth: context.screenWidth * .2,
+          leading: InkWell(
+            onTap: () => Scaffold.of(context).openDrawer(),
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                  child: Text(
+                user!.displayName![0],
+                style: kTextStyle(20),
+              )),
+            ).padAll(10),
+          ),
+          title: SearchBar(
+            controller: searchController,
+            constraints: BoxConstraints(
+              minHeight: context.screenHeight * .065,
+              minWidth: context.screenWidth * .6,
+            ),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(
+                  color: Colors.grey[900]!,
+                ),
+              ),
+            ),
+            hintText: "Search X",
+            hintStyle: MaterialStatePropertyAll(
+              kTextStyle(13, color: Colors.grey[500]),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FeatherIcons.settings,
+                size: 20,
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
